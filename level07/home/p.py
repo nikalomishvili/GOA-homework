d08b7f55f57789c96876c4bed772107ff9b271e2
nikalomishvili <nikalomishvili13@gmail.com>
2024-11-28 22:00:14 +0400

    commit

---
+++ level07/home/p.py
@@ -0,0 +1,11 @@
+
+
+name = input("enter your name:")
+last_name = input("enter your last_name:")
+age = input("enter your age:")
+centry = input("enter your centry:")
+city = input("enter your city:")
+car = input("enter your favorite car:")
+color = input("enter your favorite color")
+
+print('my name'+" "+name +" "+ "and last_name"+" "+last_name+" "+"my age"+" "+age+" "+"i liv"+' '+centry+' '+" city"+' '+city+' '+"my favorite car "+' '+car+' '+'my favorite color '+' '+color )
---
+++ level07/home/pp.py
@@ -0,0 +1,203 @@
+
+   
+        
+print("N1")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x +y)
+
+print("N2")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x +y)
+
+print("N3")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x +y)
+
+print("N4")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x +y)
+
+print("N5")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x +y)
+
+print("N6")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x +y)
+
+print("N7")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x +y)
+
+print("N8")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x +y)
+
+print("N9")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x +y)
+
+print("N10")
+x = int(input("enter x "))
+y = int(input("enter y "))
+#es aris minusi
+print('ეს არის გამოკლება')
+print("N1")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x -y)
+
+print("N2")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x -y)
+
+print("N3")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x -y)
+
+print("N4")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x -y)
+
+print("N5")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x -y)
+
+print("N6")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x -y)
+
+print("N7")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x -y)
+
+print("N8")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x -y)
+
+print("N9")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x -y)
+
+print("N10")
+x = int(input("enter x "))
+y = int(input("enter y "))
+#ეს არის გამრავლება
+print("ეს არი გამრავლება")
+print("N1")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x *y)
+
+print("N2")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x *y)
+
+print("N3")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x *y)
+
+print("N4")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x *y)
+
+print("N5")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x *y)
+
+print("N6")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x *y)
+
+print("N7")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x *y)
+
+print("N8")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x *y)
+
+print("N9")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x *y)
+
+print("N10")
+x = int(input("enter x "))
+y = int(input("enter y "))
+#ეს არის გაყოფა
+print("ეს არი გაყოფა")
+print("N1")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x //y)
+
+print("N2")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x //y)
+
+print("N3")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x //y)
+
+print("N4")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x //y)
+
+print("N5")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x //y)
+
+print("N6")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x //y)
+
+print("N7")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x //y)
+
+print("N8")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x //y)
+
+print("N9")
+x = int(input("enter x "))
+y = int(input("enter y "))
+print(x //y)
+
+print("N10")
+x = int(input("enter x "))
+y = int(input("enter y "))
+
+
